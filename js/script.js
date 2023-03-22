6798ecbb16d2494a18c40b8635f12d16700301a8
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-03-22 11:18:15 +0100

    add js folder

---
+++ js/script.js
@@ -0,0 +1,56 @@
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  const result1 = add(number1, number2);
+  alert(result1);
+
+
+  function divide(number3, number4) {
+    return number3 / number4;
+  }
+  
+  const number3 = parseInt(prompt("Enter a number:"));
+  const number4 = parseInt(prompt("Enter another number:"));
+  const result2 = divide(number3, number4);
+  alert(result2);
+
+
+  function subtract(number5, number6) {
+    return number5 - number6;
+  }
+  
+  const number5 = parseInt(prompt("Enter a number:"));
+  const number6 = parseInt(prompt("Enter another number:"));
+  const result3 = subtract(number5, number6);
+  alert(result3);
+
+
+  function multiply(number7, number8) {
+    return number7 * number8;
+  }
+  
+  const number7 = parseInt(prompt("Enter a number:"));
+  const number8 = parseInt(prompt("Enter another number:"));
+  const result4 = multiply(number7, number8);
+  alert(result4);
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
